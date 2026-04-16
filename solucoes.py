8f073fc8e18720b8cb473c5c5b1bf6c0abbf4d75
João Victor Ferreira <joaovictor2111@gmail.com>
2026-04-16 10:54:19 -0300

    feat: cifra de cesar

--- solucoes.py
+++ solucoes.py
@@ -5,8 +5,22 @@ def sao_anagramas(string1, string2):
     return sorted(normalizada1) == sorted(normalizada2)
 
 def cifra_de_cesar(texto, deslocamento):
-    # TODO: Implementar a lógica
-    pass
+    resultado = ""
+
+    for caractere in texto:
+        if "a" <= caractere <= "z":
+            novo_codigo = (ord(caractere) - ord("a") + deslocamento) % 26
+            resultado += chr(ord("a") + novo_codigo)
+        elif "A" <= caractere <= "Z":
+            novo_codigo = (ord(caractere) - ord("A") + deslocamento) % 26
+            resultado += chr(ord("A") + novo_codigo)
+        elif "0" <= caractere <= "9":
+            novo_codigo = (ord(caractere) - ord("0") + deslocamento) % 10
+            resultado += chr(ord("0") + novo_codigo)
+        else:
+            resultado += caractere
+
+    return resultado
 
 def encontrar_maior_palavra(frase):
     # TODO: Implementar a lógica
