63f6eb82331e442939b936e8586232d00b3eec5b
João Victor Ferreira <joaovictor2111@gmail.com>
2026-04-16 10:45:10 -0300

    feat: verificador de anagramas

--- solucoes.py
+++ solucoes.py
@@ -1,6 +1,11 @@
 def sao_anagramas(string1, string2):
-    # TODO: Implementar a lógica
-    pass
+    normalizada1 = "".join(char.lower() for char in string1 if char != " ")
+    normalizada2 = "".join(char.lower() for char in string2 if char != " ")
+
+    if not normalizada1 or not normalizada2:
+        return False
+
+    return sorted(normalizada1) == sorted(normalizada2)
 
 def cifra_de_cesar(texto, deslocamento):
     # TODO: Implementar a lógica
